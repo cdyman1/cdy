ed32ee3ec18bab2f0ccda34dafee23223272de6e
cdyman1 <99314937+cdyman1@users.noreply.github.com>
2022-02-17 15:12:07 +0900

    s
    a

--- js/common.js
+++ js/common.js
@@ -47,6 +47,60 @@ $(window).on("load", function(){
     }
 });
 
+// 공지 레이어 팝업
+var getCookie = function (cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+
+var setCookie = function (cname, cvalue, exdays) {
+    var todayDate = new Date();
+    todayDate.setTime(todayDate.getTime() + (exdays*24*60*60*1000));    
+    var expires = "expires=" + todayDate.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+var popNoticeClose = function() {
+    if($("input[name='chkbox']").is(":checked") == true){
+        setCookie("close","Y",1);
+    }
+    $("#pop_notice").hide();
+}
+
+$(document).ready(function() {
+    var cookiedata = document.cookie;
+    if(cookiedata.indexOf("close=Y")<0) {
+        $("#pop_notice").show();
+    } else {
+        $("#pop_notice").hide();
+    }
+    $(".close_notice").click(function() {
+        popNoticeClose();
+    });
+});
+
+// 레이어 팝업
+function popShow(el) {
+    $("body").addClass("not_scroll");
+    $(el).fadeIn(250);
+}
+
+function popHide() {
+    $("body").removeClass("not_scroll");
+}
+
+$(".pop_wrap .pop_close").on("click", function(){ // 레이어 닫기
+    $(this).closest(".pop_wrap").fadeOut(250);
+    popHide();
+});
+
+
 $(document).ready(function () {
     // AOS 설정
     AOS.init({
